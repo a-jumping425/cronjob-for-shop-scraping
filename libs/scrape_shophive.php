44a884201f574c57161accf1a913d11c8f769618
Jumping <a.jumping425@gmail.com>
2017-06-16 21:27:42 +0700

    Scrape data from shophive product page.

---
+++ libs/scrape_shophive.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Scrape the product data from shophive shop
+ */
+
+class Scrape_shophive {
+    public function get_data_in_product_page($url) {
+        try {
+//            echo '<br>get_data_in_search_page: ' . $url;
+
+            $html = \simplehtmldom_1_5\file_get_html($url);
+            $product = $html->find('div.product-shop', 0);
+
+            $min_price = 999999999999;
+            foreach( $product->find('span.price') as $span ) {
+                $price = floatval(preg_replace('/[^\d\.]+/', '', $span->plaintext));
+                if( $min_price > $price )
+                    $min_price = $price;
+            }
+
+            $availability = trim( $product->find('div.sku span', 1)->plaintext );
+
+//            echo "<br>$price, $availability";
+
+            return [$price, $availability];
+        } catch (Exception $e) {
+//            echo $e->getMessage();
+            return 0;
+        }
+    }
+
+}
