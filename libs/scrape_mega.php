f632f889adcd1b6a26b37b37d26816fb0038db4e
Jumping <a.jumping425@gmail.com>
2017-06-17 02:16:29 +0700

    Scrape the data from the product page of mega shop.

---
+++ libs/scrape_mega.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Scrape the product data from maga shop
+ */
+
+class Scrape_mega {
+    public function get_data_in_product_page($url) {
+        try {
+            echo '<br>get_data_in_search_page: ' . $url;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "cache-control: no-cache"
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+//                echo "cURL Error #:" . $err;
+                return 0;
+            }
+
+            $html = \simplehtmldom_1_5\str_get_html($response);
+            $product = $html->find('div.item-detail-right-panel', 0);
+
+            if( !$product )
+                return 0;
+
+            $price = $product->find('span.desc-price', 0)->plaintext;
+            $price = floatval(preg_replace('/[^\d\.]+/', '', $price));
+
+            $availability = trim( $product->find('div.stock-detail span', 1)->plaintext );
+
+//            echo "<br>$price, $availability";
+
+            return [$price, $availability];
+        } catch (Exception $e) {
+//            echo $e->getMessage();
+            return 0;
+        }
+    }
+
+}
