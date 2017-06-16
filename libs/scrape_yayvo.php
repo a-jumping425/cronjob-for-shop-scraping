7f84dc613e8546efa07a3ff143e79f11ec65c324
Jumping <a.jumping425@gmail.com>
2017-06-17 03:53:35 +0700

    Scrape the data from the product page of yayvo shop.

---
+++ libs/scrape_yayvo.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Scrape the product data from yayvo shop
+ */
+
+class Scrape_yayvo {
+    public function get_data_in_product_page($url) {
+        try {
+//            echo '<br>get_data_in_search_page: ' . $url;
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "GET",
+                CURLOPT_HTTPHEADER => array(
+                    "cache-control: no-cache"
+                ),
+            ));
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+//                echo "cURL Error #:" . $err;
+                return 0;
+            }
+
+            $html = \simplehtmldom_1_5\str_get_html($response);
+            $product = $html->find('div.product-shop', 0);
+
+            if( !$product )
+                return 0;
+
+            $min_price = 999999999999;
+            foreach( $product->find('span.price') as $span ) {
+                $price = substr(trim($span->plaintext), 4);
+                $price = floatval(preg_replace('/[^\d\.]+/', '', $price));
+                if( $min_price > $price )
+                    $min_price = $price;
+            }
+
+            $availability = trim( $product->find('span.quantity_amount', 0)->plaintext );
+            if( $availability != "0" )
+                $availability = "In stock";
+            else
+                $availability = "Out of stock";
+
+//            echo "<br>$price, $availability";
+
+            return [$min_price, $availability];
+        } catch (Exception $e) {
+//            echo $e->getMessage();
+            return 0;
+        }
+    }
+
+}
