55945f2da9cb67fe151eafa837723a59f2e77dad
Jumping <a.jumping425@gmail.com>
2017-06-16 16:17:12 +0700

    Get shop name from offer url.

--- cronjob-for-shop-scraping.php
+++ cronjob-for-shop-scraping.php
@@ -10,6 +10,29 @@
  */
 
 class CronjobForShopScraping {
+    private $outside_shops = [
+        'ishopping' => 'www.ishopping.pk/',
+        'shophive' => 'www.shophive.com/',
+        'daraz' => 'www.daraz.pk/',
+        'mega' => 'www.mega.pk/',
+        'homeshopping' => 'homeshopping.pk/',
+        'yayvo' => 'yayvo.com/',
+        'vmart' => 'www.vmart.pk/',
+        'telemart' => 'www.telemart.pk/',
+        'myshop' => 'myshop.pk/',
+    ];
+    private $outside_shops_search = [
+        's_ishopping' => 'www.ishopping.pk/search/?q',
+        's_shophive' => 'www.shophive.com/catalogsearch/result/?q',
+        's_daraz' => 'www.daraz.pk/catalog/?q',
+        's_mega' => 'www.mega.pk/search/',
+        // 's_homeshopping' => 'homeshopping.pk/',
+        's_yayvo' => 'yayvo.com/search/result/?q',
+        's_vmart' => 'www.vmart.pk/?subcats=Y&status=A',
+        's_telemart' => 'www.telemart.pk/catalogsearch/result/?q',
+        's_myshop' => 'myshop.pk/catalogsearch/result/?q',
+    ];
+    private $invalid_offers = [];
 
 	public function __construct() {
         // Add ajax action for cronjob
@@ -24,7 +47,7 @@ class CronjobForShopScraping {
     private function get_aps_products() {
         global $wpdb;
 
-        $sql = "SELECT p.id, p.post_title, m.`meta_value` AS offers
+        $sql = "SELECT p.ID AS id, p.post_title AS title, m.`meta_value` AS offers
                 FROM wp_posts AS p
                 INNER JOIN wp_postmeta AS m ON m.`post_id`=p.`ID` AND m.`meta_key`='aps-product-offers' AND m.`meta_value`!=''
                 WHERE p.`post_status`='publish' AND p.id IN (32935, 33835)";
@@ -32,6 +55,32 @@ class CronjobForShopScraping {
         // var_dump($products);
 
         return $products;
+    }
+
+    private function scrape_data_from_url($pid, $ptitle, $offer) {
+        $site = null;
+
+        foreach ($this->outside_shops_search as $key => $shop_url) {
+            if( strpos($offer['url'], $shop_url) !== false ) {
+                $site = [ 'shop' => $key, 'url' => $offer['url'] ];
+                break;
+            }
+        }
+
+        if($site == null) {
+            foreach ($this->outside_shops as $key => $shop_url) {
+                if( strpos($offer['url'], $shop_url) !== false ) {
+                    $site = ['shop' => $key, 'url' => $offer['url']];
+                    break;
+                }
+            }
+        }
+
+        if($site != null) {
+            var_dump($site);
+        } else {
+            $this->invalid_offers[] = ['pid' => $pid, 'ptitle' => $ptitle, 'offer_url' => $offer['url']];
+        }
     }
 
     /**
@@ -47,7 +96,10 @@ class CronjobForShopScraping {
         foreach ($products as $product) {
             $offers = unserialize($product->offers);
             if( !count($offers) ) continue;
-            var_dump($offers);
+//            var_dump($offers);
+            foreach ($offers as $offer) {
+                $data = $this->scrape_data_from_url($product->id, $product->title, $offer);
+            }
         }
 
         echo '<br>--- Ended cronjob ---';
