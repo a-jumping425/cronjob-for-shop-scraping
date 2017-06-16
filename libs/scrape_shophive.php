90e8d2094216b1a9dc15779fc108d0b0a4f26045
Jumping <a.jumping425@gmail.com>
2017-06-17 03:07:53 +0700

    Remove the code for data scraping from search product page

--- libs/scrape_shophive.php
+++ libs/scrape_shophive.php
@@ -57,30 +57,4 @@ class Scrape_shophive {
             return 0;
         }
     }
-
-    public function get_data_in_search_page($url) {
-        try {
-//            echo '<br>get_data_in_search_page: '. $url;
-
-            $html = \simplehtmldom_1_5\file_get_html($url);
-
-            $min_price = 999999999999;
-            foreach( $html->find('ul.products-grid') as $product ) {
-                foreach( $product->find('span.price') as $span ) {
-                    $price = floatval(preg_replace('/[^\d\.]+/', '', $span->plaintext));
-                    if( $min_price > $price )
-                        $min_price = $price;
-                }
-            }
-
-            $availability = "In stock";
-
-            return [$min_price, $availability];
-        } catch (Exception $e) {
-//            echo $e->getMessage();
-            return 0;
-        }
-
-        return 0;
-    }
 }
